5a542f39b761e59d759c7bb2f48878983f25b99f
MarekD97 <mdorosz2@wp.pl>
2022-07-10 14:36:27 +0200

    Add Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
