c8f1e05cd9304452861ba7b8183e36a32d7cf193
MarekD97 <mdorosz2@wp.pl>
2022-07-12 01:24:05 +0200

    Update Cart Slice

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,85 +1,87 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
+import { ProductState } from "../products/productsSlice";
 
-interface ProductState {
-    id: number,
-    thumbnail: string,
-    title: string,
-    price: number,
-    quantity: number,
+export interface CardProductState extends ProductState {
+  quantity: number;
 }
 
-interface CartState {
-    products: ProductState[],
-    totalPrice: number,
-    totalQuantity: number,
+export interface CartState {
+  products: CardProductState[];
+  totalPrice: number;
+  totalDiscountPrice: number;
+  totalQuantity: number;
+  hasDiscount: boolean;
 }
 
-// const initialState: CartState = {
-//     products: [],
-//     totalPrice: 0,
-// }
 const initialState: CartState = {
-    products: [
-        {
-          id: 1,
-          thumbnail: 'https://dummyjson.com/image/i/products/1/thumbnail.jpg',
-          title: 'iPhone 9',
-          price: 549,
-          quantity: 3
-        },
-        {
-          id: 3,
-          thumbnail: 'https://dummyjson.com/image/i/products/3/thumbnail.jpg',
-          title: 'Samsung Universe 9',
-          price: 1249,
-          quantity: 1
-        },
-        {
-          id: 4,
-          thumbnail: 'https://dummyjson.com/image/i/products/4/thumbnail.jpg',
-          title: 'OPPOF19',
-          price: 280,
-          quantity: 5
-        }
-      ],
-      totalPrice: 2078,
-      totalQuantity: 9
-}
+  products: [],
+  totalPrice: 0,
+  totalDiscountPrice: 0,
+  totalQuantity: 0,
+  hasDiscount: false,
+};
 
 export const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addProduct: (state, action: PayloadAction<ProductState>) => {
-            const currentProduct = state.products.find((item: ProductState) => item.id === action.payload.id);
+  name: "cart",
+  initialState,
+  reducers: {
+    addProduct: (state: CartState, action: PayloadAction<CardProductState>) => {
+      const currentProduct = state.products.find(
+        (item: ProductState) => item.id === action.payload.id
+      );
 
-            if(currentProduct === undefined) {
-                state.products.push(action.payload);
-            } else {
-                currentProduct.quantity += action.payload.quantity;
-            }
+      if (currentProduct === undefined) {
+        state.products.push(action.payload);
+      } else {
+        currentProduct.quantity += action.payload.quantity;
+      }
 
-            state.totalPrice += action.payload.price * action.payload.quantity;
-            state.totalQuantity += action.payload.quantity;
-        },
-        deleteProduct: (state, action) => {
-            const deletedProduct = state.products.find(product => product.id === action.payload.id);
-            
-            if(deletedProduct !== undefined) {
-                state.products = state.products.filter(product => product.id !== action.payload.id);
+      const totalPrice = action.payload.price * action.payload.quantity;
+      const discount = (totalPrice * action.payload.discountPercentage) / 100;
 
-                state.totalPrice -= deletedProduct.price * deletedProduct.quantity;
-                state.totalQuantity -= deletedProduct.quantity;
-            }
-        },
-        clearCart: (state, action) => {
-            state = initialState;
-        },
-    }
-})
+      state.totalPrice += totalPrice;
+      state.totalDiscountPrice += totalPrice - discount;
+      state.totalQuantity += action.payload.quantity;
+    },
+    deleteProduct: (
+      state: CartState,
+      action: PayloadAction<CardProductState>
+    ) => {
+      const deletedProduct = state.products.find(
+        (product) => product.id === action.payload.id
+      );
 
-export const { addProduct, deleteProduct, clearCart } = cartSlice.actions;
+      if (deletedProduct !== undefined) {
+        if (action.payload.quantity < deletedProduct.quantity) {
+          deletedProduct.quantity -= action.payload.quantity;
+        } else {
+          state.products = state.products.filter(
+            (product) => product.id !== action.payload.id
+          );
+        }
+        const totalPrice = deletedProduct.price * action.payload.quantity;
+        const discount = (totalPrice * action.payload.discountPercentage) / 100;
+
+        state.totalPrice -= totalPrice;
+        state.totalDiscountPrice -= totalPrice - discount;
+        state.totalQuantity -= action.payload.quantity;
+      }
+    },
+    setCart: (state: CartState, action: PayloadAction<CartState>) => {
+      return action.payload;
+    },
+    clearCart: (state: CartState, action: PayloadAction) => {
+      return initialState;
+    },
+    setDiscount: (state: CartState, action: PayloadAction<string>) => {
+      state.hasDiscount = action.payload === "discount";
+    },
+  },
+});
+
+export const { addProduct, deleteProduct, setCart, clearCart, setDiscount } =
+  cartSlice.actions;
 
 export const getCart = (state: RootState) => state.cart;
 
