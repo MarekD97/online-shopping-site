edfc11de2aaa65f99c33ce403a33bf769675000b
MarekD97 <mdorosz2@wp.pl>
2022-07-12 22:49:44 +0200

    Remove Counter Reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/couterSlice";
 import productsReducer from "../features/products/productsSlice";
-import cartReducer from '../features/cart/cartSlice';
+import cartReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     products: productsReducer,
     cart: cartReducer,
   },
